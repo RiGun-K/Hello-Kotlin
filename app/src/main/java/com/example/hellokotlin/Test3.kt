bcaad383694869e6c9da8ff7761794f19d60fa9d
RiGun-K <1234suho@naver.com>
2022-03-10 10:48:10 +0900

    2022/03/10 kotlin 문법 실습

--- app/src/main/java/com/example/hellokotlin/Test3.kt
+++ app/src/main/java/com/example/hellokotlin/Test3.kt
@@ -15,14 +15,52 @@ fun test2(){
 }
 
 // When 문 실습
-
 fun test3() {
-
+    val number = (-1..1).random()
+    when(number) {
+        0 ,1 -> println("$number - pass")
+        -1 -> println("$number - fail")
+    }
 
 }
 
+fun test4() {
+    val score = (0..100).random()
+    val grade = when(score){
+        in 90..100 -> "A"
+        in 80..90 -> "B"
+        in 70..80 -> "C"
+        in 60..70 -> "D"
+        else -> "F"
+    }
+    println("Score:$score, Grade:$grade")
+}
+
+fun test5() {
+    val test_5 = (-1..1).random()
+    val str = when(test_5) {
+        -1 -> "negative"
+        0 -> "zero"
+        else -> "positive"
+    }
+    println(str)
+}
+
+fun test6() {
+    val array = arrayOf(1, 2, 3, "a", "b", "c")
+    array.forEach { it -> when(it){
+        is String -> println("$it is a String")
+        else -> println("$it is a Integer")
+        }
+    }
+}
+
 fun main(){
-    test1()
-    test2()
+//    test1()
+//    test2()
+//    test3()
+//    test4()
+//    test5()
+    test6()
 }
 
